07f1320bd68470715b19d09f5a2d53706df8b685
WeeJeeja <saoirsemccann11@hotmail.com>
2017-03-19 18:40:24 +0000

    Updates for resource chart

--- PresentationLayer/Controllers/ChartSampleController.cs
+++ PresentationLayer/Controllers/ChartSampleController.cs
@@ -33,7 +33,7 @@ namespace PresentationLayer.Controllers
         public ActionResult Index()
         {
             //create a collection of data
-            
+
             //modify data type to make it of array type
             var test = new List<ChartData>
             {
@@ -79,7 +79,7 @@ namespace PresentationLayer.Controllers
             var yDataFrequency = test.Select(i => new object[] { i.Frequency }).ToArray();
 
             var yDataOccupancy = test.Select(i => new object[] { i.Occupancy }).ToArray();
-            
+
             var yDataUtilisation = test.Select(i => new object[] { i.Utilisation }).ToArray();
 
             //instanciate an object of the Highcharts type
@@ -183,6 +183,97 @@ namespace PresentationLayer.Controllers
         }
 
         //
+        // GET: /ChartSample/Details/5
+
+        public ActionResult Test()
+        {
+            //instanciate an object of the Highcharts type
+
+            object[][] series = new object[][] { new object[] { 0, 0, 10 }, new object[] { 0, 1, 19 }, new object[] { 0, 2, 8 }, new object[] { 0, 3, 24 }, new object[] { 0, 4, 67 }, new object[] { 1, 0, 92 }, new object[] { 1, 1, 58 }, new object[] { 1, 2, 78 }, new object[] { 1, 3, 117 }, new object[] { 1, 4, 48 }, new object[] { 2, 0, 35 }, new object[] { 2, 1, 15 }, new object[] { 2, 2, 123 }, new object[] { 2, 3, 64 }, new object[] { 2, 4, 52 }, new object[] { 3, 0, 72 }, new object[] { 3, 1, 132 }, new object[] { 3, 2, 114 }, new object[] { 3, 3, 19 }, new object[] { 3, 4, 16 }, new object[] { 4, 0, 38 }, new object[] { 4, 1, 5 }, new object[] { 4, 2, 8 }, new object[] { 4, 3, 117 }, new object[] { 4, 4, 115 }, new object[] { 5, 0, 88 }, new object[] { 5, 1, 32 }, new object[] { 5, 2, 12 }, new object[] { 5, 3, 6 }, new object[] { 5, 4, 120 }, new object[] { 6, 0, 13 }, new object[] { 6, 1, 44 }, new object[] { 6, 2, 88 }, new object[] { 6, 3, 98 }, new object[] { 6, 4, 96 }, new object[] { 7, 0, 31 }, new object[] { 7, 1, 1 }, new object[] { 7, 2, 82 }, new object[] { 7, 3, 32 }, new object[] { 7, 4, 30 }, new object[] { 8, 0, 85 }, new object[] { 8, 1, 97 }, new object[] { 8, 2, 123 }, new object[] { 8, 3, 64 }, new object[] { 8, 4, 84 }, new object[] { 9, 0, 47 }, new object[] { 9, 1, 114 }, new object[] { 9, 2, 31 }, new object[] { 9, 3, 48 }, new object[] { 9, 4, 91 } };
+
+            Series Hestavollane = new Series
+            {
+                Name = "Test",
+                Data = new Data(series)
+            };
+
+            var chart = new Highcharts("chart")
+                //define the type of chart 
+                        .InitChart(new Chart { Type = ChartTypes.Heatmap, DefaultSeriesType = ChartTypes.Heatmap })
+                //overall Title of the chart 
+                        .SetTitle(new Title { Text = "Test heatmap" })
+                //small label below the main Title
+                        .SetSubtitle(new Subtitle { Text = "Subtitle" })
+                //load the X values
+                        .SetXAxis(new XAxis { Categories = new String[] { "Alexander", "Marie", "Maximilian", "Sophia", "Lukas", "Maria", "Leon", "Anna", "Tim", "Laura" }})
+                //set the Y title
+                .SetPlotOptions(new PlotOptions
+                {
+                    Heatmap = new PlotOptions
+                    {
+                        Heatmap = new YAxisPlotBands
+                        {
+                            Events
+                        }
+                    }
+                    Line = new PlotOptionsLine
+                    {
+                        DataLabels = new PlotOptionsLineDataLabels
+                        {
+                            Enabled = true
+                        },
+                        EnableMouseTracking = false
+                    }
+                })
+                        .SetYAxis(new YAxis { Title = new YAxisTitle { Text = "Rate %" },
+                            Categories = new String[] { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday" }
+                            
+                        })
+    //                    .SetLegend(new Legend
+    //                    {
+    //                        M
+    //                    })
+
+
+
+    //                    colorAxis: {
+    //    min: 0,
+    //    minColor: '#FFFFFF',
+    //    maxColor: Highcharts.getOptions().colors[0]
+    //},
+
+    //legend: {
+    //    align: 'right',
+    //    layout: 'vertical',
+    //    margin: 0,
+    //    verticalAlign: 'top',
+    //    y: 25,
+    //    symbolHeight: 280
+    //},
+                        .SetTooltip(new Tooltip
+                        {
+                            Enabled = true,
+                            Formatter = @"function () { return '<b>' + this.series.xAxis.categories[this.point.x] + '</b> sold <br><b>'+ this.point.value + '</b> items on <br><b>' + this.series.yAxis.categories[this.point.y] + '</b>'; }",
+                        })
+                        .SetSeries(new[] {
+                            Hestavollane,
+                        });
+
+            return View(chart);
+        }
+
+
+        //
+        // GET: /ChartSample/Details/5
+
+        public ActionResult Test2()
+        {
+            
+
+            return View();
+        }
+
+        //
         // POST: /ChartSample/Edit/5
 
         [HttpPost]
@@ -192,7 +283,7 @@ namespace PresentationLayer.Controllers
             {
                 // TODO: Add update logic here
 
-                return RedirectToAction("Index");
+                return View();
             }
             catch
             {
--- PresentationLayer/Models/GroupBooking.cs
+++ PresentationLayer/Models/GroupBooking.cs
@@ -9,9 +9,10 @@ namespace PresentationLayer.Models
     {
         public GroupBooking()
         {
-            Attendees     = new List<User>();
-            Teams         = new List<Team>();
-            SelectedTeams = new List<string>();
+            Attendees         = new List<User>();
+            Teams             = new List<Team>();
+            SelectedAttendees = new List<string>();
+            SelectedTeams     = new List<string>();
         }
         public DateTime Date { get; set; }
 
@@ -21,9 +22,11 @@ namespace PresentationLayer.Models
 
         public ICollection<User> Attendees { get; set; }
 
-        public IList<string> SelectedTeams { get; set; }
+        public IList<string> SelectedAttendees { get; set; }
 
         public ICollection<Team> Teams { get; set; }
+
+        public IList<string> SelectedTeams { get; set; }
 
         public int Capacity { get; set; }
     }
