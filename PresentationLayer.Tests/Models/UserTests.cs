ca1016452a3b81f19643ee132fbad4cafc8f6ade
WeeJeeja <saoirsemccann11@hotmail.com>
2017-05-01 16:29:13 +0100

    Added NUnit and NUnit adapter

---
+++ PresentationLayer.Tests/Models/UserTests.cs
@@ -0,0 +1,78 @@
+﻿using NUnit.Framework;
+using PresentationLayer.Models;
+using System;
+
+namespace PresentationLayer.Tests.Models
+{
+    [TestFixture]
+    public class UserTests
+    {
+        [Test]
+        public void UserEmployeeNumber()
+        {
+            var model = new User()
+            {
+                EmployeeNumber = 10000001,
+                Forename       = "Saoirse",
+                Surname        = "McCann",
+                Email          = "smccann60@qub.ac.uk",
+                JobTitle       = "Placement Software Engineer",
+            };
+
+            var results = model;
+
+            Assert.AreEqual(10000001, results.EmployeeNumber);
+        }
+
+        [Test]
+        public void UserForename()
+        {
+            var model = new User()
+            {
+                EmployeeNumber = 10000001,
+                Forename = "Saoirse",
+                Surname  = "McCann",
+                Email    = "smccann60@qub.ac.uk",
+                JobTitle = "Placement Software Engineer",
+            };
+
+            var results = model;
+
+            Assert.AreEqual("Saoirse", results.Forename);
+        }
+
+        [Test]
+        public void UserSurname()
+        {
+            var model = new User()
+            {
+                EmployeeNumber = 10000001,
+                Forename = "Saoirse",
+                Surname = "McCann",
+                Email = "smccann60@qub.ac.uk",
+                JobTitle = "Placement Software Engineer",
+            };
+
+            var results = model;
+
+            Assert.AreEqual("McCann", results.Surname);
+        }
+
+        [Test]
+        public void UserEmail()
+        {
+            var model = new User()
+            {
+                EmployeeNumber = 10000001,
+                Forename = "Saoirse",
+                Surname = "McCann",
+                Email = "smccann60@qub.ac.uk",
+                JobTitle = "Placement Software Engineer",
+            };
+
+            var results = model;
+
+            Assert.AreEqual("smccann60@qub.ac.uk", results.Email);
+        }
+    }
+}
--- PresentationLayer.Tests/Properties/AssemblyInfo.cs
+++ PresentationLayer.Tests/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("PresentationLayer.Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("772eefee-44d4-4dcc-a0e3-07acf1556447")]
+[assembly: Guid("a1afc851-a9f6-4383-81c4-f6394519364a")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,7 +29,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
