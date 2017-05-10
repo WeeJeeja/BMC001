7678a14be6a3abab73d0180fc3336c27dd99ed36
WeeJeeja <saoirsemccann11@hotmail.com>
2017-05-10 21:52:18 +0100

    Rename chart to dashboard

---
+++ PresentationLayer/HelperMethods/DateCalculations.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace PresentationLayer.HelperMethods
+{
+    public class DateCalculations
+    {
+        /// <summary>
+        /// Finds the week starting date for given date
+        /// </summary>
+        /// <param name="date"></param>
+        /// <returns></returns>
+        public DateTime FindStartDate(DateTime date)
+        {
+            switch (date.DayOfWeek)
+            {
+                case DayOfWeek.Monday:
+                    {
+                        break;
+                    }
+                case DayOfWeek.Tuesday:
+                    {
+                        date = date.AddDays(-1);
+                        break;
+                    }
+                case DayOfWeek.Wednesday:
+                    {
+                        date = date.AddDays(-2);
+                        break;
+                    }
+                case DayOfWeek.Thursday:
+                    {
+                        date = date.AddDays(-3);
+                        break;
+                    }
+                case DayOfWeek.Friday:
+                    {
+                        date = date.AddDays(-4);
+                        break;
+                    }
+                case DayOfWeek.Saturday:
+                    {
+                        date = date.AddDays(-5);
+                        break;
+                    }
+                case DayOfWeek.Sunday:
+                    {
+                        date = date.AddDays(-6);
+                        break;
+                    }
+            }
+
+            return date;
+        }
+    }
+}
